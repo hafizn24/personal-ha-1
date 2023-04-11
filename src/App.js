c723be0a672cea0eca68cd5af554858905bd69ad
hafiz <hafizn24@gmail.com>
2023-04-11 18:33:50 +0800

    Alpha Release

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import Typewriter from 'typewriter-effect';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const text1 = "Dear Anisha,"
+  const text2 = "Let's be honest, the last month is a rollercoaster for us. There are a lot of things happening but thanks to Allah that our relationship still strong. As you know about me I'm not good with my words, so I would like to keep it short and simple. Thanks for believe in me for the last 30 days. It means a lot for me and I hope that we will be together until jannah. Happy Monthsary. ❤️"
+
+  const font = "'Cutive Mono', monospace"
+  const fontTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+    typography: {
+      fontFamily: font,
+    },
+  })
+
+  function funcType1() {
+    return (
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString(text1)
+            .changeDelay("natural")
+            .start();
+        }}
+      />
+    )
+  }
+
+  const DelayComponent = () => {
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setShow(true)
+      }, 2300)
+
+      return () => clearTimeout(timeout)
+
+    }, [show])
+
+    if (!show) return null
+
+      return (
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(text2)
+              .changeDelay("natural")
+              .start();
+          }}
+        />
+      )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={fontTheme}>
+      <CssBaseline enableColorScheme />
+      <Box sx={{ my: 5 }}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h2>{funcType1()}</h2>
+          <Box sx={{ px: 3 }}>
+            <h3>{DelayComponent()}</h3>
+          </Box>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 }
 
